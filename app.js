d9c24224a4590d7b23ffbaafe01664c39ecfed3b
mohamedtarekabdelhamid <mohamedtarekabdelhamid@gmail.com>
2023-07-07 19:31:47 +0200

    Refactoring mounting routes

--- app.js
+++ app.js
@@ -6,13 +6,7 @@ const morgan = require('morgan')
 const {globalError} = require('./middlewares/errorMiddleware')
 const dbConnection = require('./database/database')
 
-const authRoute = require('./routes/authRoute')
-const categoryRoute = require('./routes/categoryRoute')
-const subCategoryRoute = require('./routes/subCategoryRoute')
-const brandRoute = require('./routes/brandRoute')
-const productRoute = require('./routes/productRoute')
-const userRoute = require('./routes/userRoute')
-const reviewRoute = require('./routes/reviewRoute')
+const mountRoutes = require('./routes')
 
 const mode = process.env.NODE_ENV
 const port = process.env.PORT
@@ -28,16 +22,10 @@ if (mode === 'development') {
     app.use(morgan('dev'))
 }
 
-app.use('/api/v1/auth', authRoute)
-app.use('/api/v1/categories', categoryRoute)
-app.use('/api/v1/subcategories', subCategoryRoute)
-app.use('/api/v1/brands', brandRoute)
-app.use('/api/v1/products', productRoute)
-app.use('/api/v1/users', userRoute)
-app.use('/api/v1/reviews', reviewRoute)
+mountRoutes(app)
 
 app.use(globalError)
 
 app.listen(port, () => {
     console.log(`App is running on port ${port}`)
-});
+})
---
+++ routes/index.js
@@ -0,0 +1,19 @@
+const authRoute = require('./authRoute')
+const categoryRoute = require('./categoryRoute')
+const subCategoryRoute = require('./subCategoryRoute')
+const brandRoute = require('./brandRoute')
+const productRoute = require('./productRoute')
+const userRoute = require('./userRoute')
+const reviewRoute = require('./reviewRoute')
+
+const mountRoutes = (app) => {
+    app.use('/api/v1/auth', authRoute)
+    app.use('/api/v1/categories', categoryRoute)
+    app.use('/api/v1/subcategories', subCategoryRoute)
+    app.use('/api/v1/brands', brandRoute)
+    app.use('/api/v1/products', productRoute)
+    app.use('/api/v1/users', userRoute)
+    app.use('/api/v1/reviews', reviewRoute)
+}
+
+module.exports = mountRoutes
